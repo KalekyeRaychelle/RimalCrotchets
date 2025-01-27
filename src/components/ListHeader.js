e2eb1c96f2665ecd9083bd49df87f5a6c64839a6
raychelleKalekye <kalekyeraychelle@gmail.com>
2025-01-27 16:24:11 +0300

    Added dropdown menu to smaller screens

---
+++ src/components/ListHeader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/ListHeader.css'
+const ListHeader = () => {
+  
+  return (
+    <div className='Dropdown'>
+        <ul className='dropdownList'>
+            <li>Sweaters</li>
+            <li>Accesories</li>
+            <li>Pants</li>
+            <li>Tops</li>
+            <li>Bags</li>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default ListHeader
+
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import headerIcon from '../assets/headerIcon.png';
 import { FaShoppingBasket } from "react-icons/fa";
 import { FaSignInAlt } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
+import { FaListUl } from "react-icons/fa";
+import ListHeader from './ListHeader';
 import '../styles/header.css';
 
-const Header = () => {
-  return (
+const Header= () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(prevState => !prevState);
+  };
+
+  return(
     <div className='Header'>
-      <div className='leftSide'>
-        <img src={headerIcon} alt='Header Icon' />
-        <h2>RIMAL'S CROTCHETS</h2>
-      </div>
-      <div className='searchBar'>
-        <form>
-          <input 
-            type="search" 
-            placeholder="SEARCH PIECE" 
-            id="searchBar"
-          />
-          <FaSearch className="searchIcon" />
-        </form>
-      </div>
-      <div className='rightSide'>
-        <ul className='liLinks'>
-          <li><FaShoppingBasket /></li>
-          <li><FaSignInAlt /></li>
-        </ul>
-      </div> 
+    <div className='leftSide'>
+      <img src={headerIcon} alt='Header Icon' />
+      <h2>RIMAL'S CROTCHETS</h2>
     </div>
+    <div className='searchBar'>
+      <form>
+        <input 
+          type="search" 
+          placeholder="SEARCH PIECE" 
+          id="searchBar"
+        />
+        <FaSearch className="searchIcon" />
+      </form>
+    </div>
+    <div className='rightSide'>
+      <ul className='liLinks'>
+            <li><FaShoppingBasket /></li>
+            <li><FaSignInAlt /></li>
+            <li onClick={toggleDropdown} className="dropdownIcon">
+            <FaListUl /></li>
+      </ul>
+      {showDropdown && <ListHeader />}
+    </div> 
+  </div>
   );
 }
-
-export default Header;
+export default Header
