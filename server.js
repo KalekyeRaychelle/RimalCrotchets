c1201f85fed672fbdaa83b68de9962d87884724a
raychelleKalekye <kalekyeraychelle@gmail.com>
2025-01-30 11:04:02 +0300

    Connecting backend with frontend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express= require('express')
 const app=express()
+const cors = require("cors");
+app.use(cors({ origin: "http://localhost:5900" }));
+app.use(express.json()); 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the backend!" });
+});
 
-app.get('/',(req,res)=>{
-    console.log("Here")
-    res.send("Hi")
-})
 app.listen(7700)
+
