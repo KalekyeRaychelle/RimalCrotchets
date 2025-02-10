b6d94c63fd6d1b6892a65def7541aa7b9cecbca9
raychelleKalekye <kalekyeraychelle@gmail.com>
2025-02-10 16:29:01 +0300

    Added cart route to icon

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ const Header= () => {
   const toggleDropdown = () => {
     setShowDropdown(prevState => !prevState);
   };
-
+  
   return(
     <div className='Header'>
     <div className='leftSide'>
@@ -33,7 +33,7 @@ const Header= () => {
     </div>
     <div className='rightSide'>
       <ul className='liLinks'>
-            <li><Link><FaShoppingBasket /></Link></li>
+            <li><Link to='/ShoppingCart'><FaShoppingBasket /></Link></li>
             <li><Link to='/SignUp'><FaSignInAlt /></Link></li>
             <li onClick={toggleDropdown} className="dropdownIcon">
             <FaListUl /></li>
